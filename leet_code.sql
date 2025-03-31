fc27829c425299e73c53d894a27733517989254f
Zofia S <zosiasadel11@gmail.com>
2025-03-31 14:32:30 +0200

    problem 595

--- leet_code.sql
+++ leet_code.sql
@@ -6,3 +6,7 @@ WHERE low_fats = 'Y' AND recyclable = 'Y';
 SELECT name
 FROM Customer
 WHERE referee_id IS NULL OR referee_id <> 2;
+--595. Big Countries
+SELECT name,population,area
+FROM World
+WHERE area>= 3000000 OR population>=25000000;

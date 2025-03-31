aa7dc97c8d1776c60e878ba7f48068e310fa758e
Zofia S <zosiasadel11@gmail.com>
2025-03-31 15:20:15 +0200

    1581. Customer Who Visited but Did Not Make Any Transactions

--- basic_joins.sql
+++ basic_joins.sql
@@ -8,3 +8,10 @@ SELECT Product.product_name, Sales.year, Sales.price
 FROM Sales
 LEFT JOIN Product
 On Sales.product_id = Product.product_id;
+--1581. Customer Who Visited but Did Not Make Any Transactions
+SELECT Visits.customer_id, COUNT(Visits.visit_id) AS count_no_trans
+FROM Visits
+LEFT JOIN Transactions 
+ON Visits.visit_id = Transactions.visit_id
+WHERE Transactions.transaction_id IS NULL
+GROUP BY Visits.customer_id;

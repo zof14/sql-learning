69fcfe507b51b0cfbe323b587e6de12ba6aabe92
Zofia S <zosiasadel11@gmail.com>
2025-03-31 14:57:55 +0200

    1068. Product Sales Analysis I

--- basic_joins.sql
+++ basic_joins.sql
@@ -3,3 +3,8 @@ SELECT name, EmployeeUNI.unique_id
 FROM Employees
 LEFT JOIN EmployeeUNI
 ON Employees.id = EmployeeUNI.id;
+--1068. Product Sales Analysis I
+SELECT Product.product_name, Sales.year, Sales.price
+FROM Sales
+LEFT JOIN Product
+On Sales.product_id = Product.product_id;

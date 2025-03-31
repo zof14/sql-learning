1e9699c4b34a8575586b8a9e59bbe112ee30063e
Zofia S <zosiasadel11@gmail.com>
2025-03-31 14:39:29 +0200

    1148. Article Views I

--- leet_code.sql
+++ leet_code.sql
@@ -10,3 +10,8 @@ WHERE referee_id IS NULL OR referee_id <> 2;
 SELECT name,population,area
 FROM World
 WHERE area>= 3000000 OR population>=25000000;
+--1148. Article Views I
+SELECT DISTINCT author_id AS id
+FROM Views
+WHERE author_id = viewer_id
+ORDER BY author_id ASC;

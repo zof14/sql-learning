e07b0af065f04bdfa71931d3411c885b6a4de6c9
Zofia S <zosiasadel11@gmail.com>
2025-03-31 22:55:44 +0200

    1251. Average Selling Price

--- basic_aggregate.sql
+++ basic_aggregate.sql
@@ -6,3 +6,12 @@ SELECT *
 FROM Cinema
 WHERE id%2!=0 AND description!= 'boring'
 ORDER BY rating DESC;
+
+--1251. Average Selling Price
+SELECT p.product_id, 
+       ROUND(SUM(p.price * u.units) / NULLIF(SUM(u.units), 0), 2)AS average_price
+FROM Prices p
+LEFT JOIN UnitsSold u 
+ON p.product_id = u.product_id
+AND u.purchase_date BETWEEN p.start_date AND p.end_date
+GROUP BY p.product_id;

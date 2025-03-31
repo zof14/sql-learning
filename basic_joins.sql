fee65f57445e1072554f3f10e9ed77aef80cd0ce
Zofia S <zosiasadel11@gmail.com>
2025-03-31 16:21:59 +0200

    1661. Average Time of Process per Machine

--- basic_joins.sql
+++ basic_joins.sql
@@ -25,3 +25,13 @@ FROM Weather w1
 JOIN Weather w2
 ON w1.recordDate = w2.recordDate + INTERVAL 1 DAY
 WHERE w1.temperature > w2.temperature;
+--1661. Average Time of Process per Machine
+SELECT a.machine_id, 
+       ROUND(AVG(b.timestamp - a.timestamp), 3) AS processing_time
+FROM Activity a
+JOIN Activity b
+ON a.machine_id = b.machine_id 
+AND a.process_id = b.process_id 
+AND a.activity_type = 'start' 
+AND b.activity_type = 'end'
+GROUP BY a.machine_id;

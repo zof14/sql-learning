97a868730ff1df413ad8d7225430644e0a8def5c
Zofia S <zosiasadel11@gmail.com>
2025-04-08 16:37:12 +0200

    596. Classes More Than 5 Students

--- sorting_grouping.sql
+++ sorting_grouping.sql
@@ -14,3 +14,22 @@ FROM Activity
 WHERE activity_date BETWEEN  '2019-06-28' AND '2019-07-27'
 GROUP BY activity_date
 ORDER BY activity_date;
+--1070. Product Sales Analysis III
+SELECT s.product_id, 
+       first_sales.first_year, 
+       s.quantity, 
+       s.price
+FROM Sales s
+JOIN (
+    SELECT product_id, MIN(year) AS first_year
+    FROM Sales
+    GROUP BY product_id
+) AS first_sales
+  ON s.product_id = first_sales.product_id
+  AND s.year = first_sales.first_year;
+--596. Classes More Than 5 Students
+SELECT class 
+FROM Courses
+
+GROUP BY class
+HAVING COUNT(*)>=5;

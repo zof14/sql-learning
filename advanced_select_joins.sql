7c3562bc9ebde3d21911884f424eea5b26c9238f
Zofia S <zosiasadel11@gmail.com>
2025-04-08 17:44:01 +0200

    1789. Primary Department for Each Employee

--- advanced_select_joins.sql
+++ advanced_select_joins.sql
@@ -5,3 +5,26 @@ JOIN Employees e2
 ON e1.employee_id = e2.reports_to
 GROUP BY e1.employee_id
 ORDER BY e1.employee_id;
+
+--1789. Primary Department for Each Employee
+SELECT employee_id, department_id
+FROM Employee
+WHERE primary_flag = 'Y'
+OR employee_id IN(
+    SELECT employee_id
+    FROM Employee
+    GROUP BY employee_id
+    HAVING COUNT(*) = 1
+
+);
+--OR
+SELECT employee_id, department_id
+FROM Employee
+WHERE primary_flag = 'Y'
+
+UNION
+
+SELECT employee_id, department_id
+FROM Employee
+GROUP BY employee_id, department_id
+HAVING COUNT(*) = 1;

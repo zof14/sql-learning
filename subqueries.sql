291d6dba7ad9691acc67e2e55eb0d52be74efa4b
Zofia S <zosiasadel11@gmail.com>
2025-04-08 23:24:08 +0200

    626. Exchange Seats

--- subqueries.sql
+++ subqueries.sql
@@ -4,3 +4,11 @@ FROM Employees
 WHERE salary<30000 AND manager_id IS NOT NULL AND manager_id NOT IN (SELECT
 employee_id FROM Employees)
 ORDER BY employee_id;
+
+--626. Exchange Seats
+SELECT CASE WHEN id%2 = 1 AND id+1<=(SELECT MAX(id) FROM Seat) THEN id+1
+ WHEN id%2 = 0  THEN id-1
+ELSE id
+END AS id, student
+FROM Seat
+ORDER BY id ASC;

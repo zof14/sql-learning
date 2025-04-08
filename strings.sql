0f20dda57e1b1e48f012287c4fc6f6e9f382d746
Zofia S <zosiasadel11@gmail.com>
2025-04-08 23:49:43 +0200

    196. Delete Duplicate Emails

--- strings.sql
+++ strings.sql
@@ -8,3 +8,9 @@ ORDER BY user_id ASC;
 SELECT patient_id,patient_name,conditions
 FROM Patients
 WHERE conditions LIKE '% DIAB1' OR conditions LIKE '% DIAB1%' OR conditions LIKE 'DIAB1%';
+
+--196. Delete Duplicate Emails
+DELETE p1 
+FROM Person p1
+JOIN Person p2
+ON p1.id>p2.id AND p1.email=p2.email;

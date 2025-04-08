b9f62a6cf800d30b70a9e28891fe64bacefb2269
Zofia S <zosiasadel11@gmail.com>
2025-04-08 18:03:28 +0200

    180. Consecutive Numbers

--- advanced_select_joins.sql
+++ advanced_select_joins.sql
@@ -32,3 +32,11 @@ HAVING COUNT(*) = 1;
 --610. Triangle Judgement
 SELECT x,y,z,(CASE WHEN x+y>z AND x+z>y AND z+y>x THEN 'Yes' ELSE 'No' END) AS triangle
 FROM Triangle;
+--180. Consecutive Numbers
+SELECT  DISTINCT l1.num  AS ConsecutiveNums
+FROM Logs l1
+JOIN Logs l2 
+ON l1.id = l2.id - 1
+JOIN Logs l3 
+ON l1.id = l3.id - 2
+WHERE l1.num = l2.num AND l1.num = l3.num;

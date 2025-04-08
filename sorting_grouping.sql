7b435fa49fca4780278435a154f588ab9d21c3ea
Zofia S <zosiasadel11@gmail.com>
2025-04-08 16:21:41 +0200

    1141. User Activity for the Past 30 Days I

--- sorting_grouping.sql
+++ sorting_grouping.sql
@@ -2,3 +2,15 @@
 SELECT teacher_id, COUNT(DISTINCT subject_id) AS cnt
 FROM Teacher
 GROUP BY teacher_id;
+--1141. User Activity for the Past 30 Days I
+
+SELECT activity_date AS day, COUNT(DISTINCT user_id) as active_users
+FROM Activity
+WHERE activity_date BETWEEN (DATE('2019-07-27') - INTERVAL 30 DAY) AND DATE('2019-07-27')
+GROUP BY activity_date;
+
+SELECT activity_date AS day, COUNT(DISTINCT user_id) as active_users
+FROM Activity
+WHERE activity_date BETWEEN  '2019-06-28' AND '2019-07-27'
+GROUP BY activity_date
+ORDER BY activity_date;

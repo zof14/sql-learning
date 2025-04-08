0cb3ec51c6c5ff1297b65a402c0595c955971e10
Zofia S <zosiasadel11@gmail.com>
2025-04-08 23:45:36 +0200

    --1527. Patients With a Condition

--- strings.sql
+++ strings.sql
@@ -2,3 +2,9 @@
 SELECT user_id, CONCAT(UPPER(LEFT(name,1)),LOWER(SUBSTRING(name,2))) AS name
 FROM Users
 ORDER BY user_id ASC;
+
+
+--1527. Patients With a Condition
+SELECT patient_id,patient_name,conditions
+FROM Patients
+WHERE conditions LIKE '% DIAB1' OR conditions LIKE '% DIAB1%' OR conditions LIKE 'DIAB1%';

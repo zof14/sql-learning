ab1de22a3ee5e1e0a49fe54ed618ed6569f828bb
Zofia S <zosiasadel11@gmail.com>
2025-04-08 22:54:42 +0200

    1978. Employees Whose Manager Left the Company

--- advanced_select_joins.sql
+++ advanced_select_joins.sql
@@ -66,3 +66,10 @@ FROM(
 WHERE total_weight<=1000
 ORDER BY total_weight DESC
 LIMIT 1;
+
+--1978. Employees Whose Manager Left the Company
+SELECT employee_id 
+FROM Employees
+WHERE salary<30000 AND manager_id IS NOT NULL AND manager_id NOT IN (SELECT
+employee_id FROM Employees)
+ORDER BY employee_id;

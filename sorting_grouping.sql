f91331bdb0986457e8263b88f89220ff9d826a2e
Zofia S <zosiasadel11@gmail.com>
2025-04-08 16:42:18 +0200

    1729. Find Followers Count

--- sorting_grouping.sql
+++ sorting_grouping.sql
@@ -33,3 +33,9 @@ FROM Courses
 
 GROUP BY class
 HAVING COUNT(*)>=5;
+--1729. Find Followers Count
+SELECT user_id, COUNT(follower_id) as followers_count
+FROM Followers
+GROUP BY user_id
+ORDER BY user_id ASC;
+aa

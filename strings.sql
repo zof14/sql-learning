c7841638a8a4aaa53040e6a1ce32f5f35d250a1d
Zofia S <zosiasadel11@gmail.com>
2025-04-11 21:49:37 +0200

    1327. List the Products Ordered in a Period

--- strings.sql
+++ strings.sql
@@ -30,3 +30,12 @@ SELECT  sell_date, COUNT(DISTINCT product) as num_sold,GROUP_CONCAT(DISTINCT pro
 FROM Activities
 GROUP BY sell_date
 ORDER BY sell_date;
+--1327. List the Products Ordered in a Period
+SELECT p.product_name, SUM(unit) as unit
+FROM Products p
+JOIN Orders o
+ON p.product_id = o.product_id
+WHERE o.order_date BETWEEN '2020-02-01' AND '2020-02-29'
+GROUP BY product_name
+HAVING SUM(unit)>=100
+;

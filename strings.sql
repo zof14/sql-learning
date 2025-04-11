669bfec50aa56e3a1324d1c7463fd7837ccdc761
Zofia S <zosiasadel11@gmail.com>
2025-04-11 21:32:12 +0200

    1484. Group Sold Products By The Date

--- strings.sql
+++ strings.sql
@@ -23,3 +23,10 @@ SELECT (
         FROM Employee
     )
 ) AS SecondHighestSalary;
+
+--1484. Group Sold Products By The Date
+
+SELECT  sell_date, COUNT(DISTINCT product) as num_sold,GROUP_CONCAT(DISTINCT product ORDER BY product SEPARATOR ',') AS products
+FROM Activities
+GROUP BY sell_date
+ORDER BY sell_date;

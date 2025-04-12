555f9beae4d41824743114552cc789a6cc57618e
Zofia S <zosiasadel11@gmail.com>
2025-04-12 17:24:02 +0200

    1321. Restaurant Growth

--- subqueries.sql
+++ subqueries.sql
@@ -36,3 +36,18 @@ LIMIT 1
 );
 
  
+--1321. Restaurant Growth
+SELECT c1.visited_on, SUM(c2.amount) AS amount, ROUND(SUM(c2.amount)/7,2) AS average_amount
+FROM (
+    SELECT visited_on, SUM(amount) AS amount
+    FROM Customer
+    GROUP BY visited_on
+) AS c1
+JOIN 
+(SELECT visited_on, SUM(amount) AS amount
+FROM Customer
+GROUP BY visited_on) AS c2
+ON c2.visited_on BETWEEN c1.visited_on - INTERVAL 6 DAY AND c1.visited_on
+GROUP BY c1.visited_on
+HAVING COUNT(*)=7
+ORDER BY c1.visited_on;
